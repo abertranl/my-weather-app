050ba2f44dcb956fec5ec164c6feea97f330aa2a
Andrea Bertran <bertran.andrea@gmail.com>
2025-09-24 17:40:53 +0200

    Wind, humidity & icon updates

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#current-temperature");
-  let temperature = Math.round(response.data.temperature.current);
+function displayNewInfo(response) {
+  let temperatureElement = document.querySelector("#current-temperature-value");
   let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#current-description");
+  let humidityElement = document.querySelector("#current-humidity");
+  let windElement = document.querySelector("#current-wind");
+  let iconElement = document.querySelector("#current-temperature-icon");
+
   cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = temperature;
-}
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let city = searchInputElement.value;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />`;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let description = response.data.condition.description;
+  let capitalizedDescription =
+    description.charAt(0).toUpperCase() + description.slice(1);
+  descriptionElement.innerHTML = capitalizedDescription;
 
-  axios.get(apiUrl).then(displayTemperature);
+  console.log(response.data);
 }
 
 function formatDate(date) {
@@ -44,8 +48,31 @@ function formatDate(date) {
   return `${formattedDay} ${hours}:${minutes}`;
 }
 
+function phraseUpdate(event) {
+  event.preventDefault();
+  let phraseElement = document.querySelector("#new-phrase");
+  phraseElement.textContent = ". Dress it like you mean it!";
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+  searchCity(city);
+}
+
+function searchCity(city) {
+  let apiKey = "of26b55307144beab6f326a4tefcaa5e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayNewInfo);
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
+searchForm.addEventListener("submit", phraseUpdate);
+
+searchCity("Madrid");
 
 let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
